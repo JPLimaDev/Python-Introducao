0fc39702db093152921a98d30ca44cf5258d746d
joaopaulo <joaopaulodamotalima@hotmail.com>
2026-01-17 17:43:18 -0300

    Enviando Inicio de POO

---
+++ POO/1_modelagem.py
@@ -0,0 +1,16 @@
+class Pessoas:
+    possui_olho = True
+    possui_boca = True
+    raca = "Ser Humano"
+    def __init__(self, nome, idade):
+        self.nome = nome 
+        self.idade = idade
+
+
+    def retorna_nome(self):
+        return self.nome
+    
+    def logar_sistema(self):
+        print(f'{self.retorna_nome()} está logando no sistema')
+
+print(Pessoas.possui_olho) 
---
+++ POO/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+        self.nome = nome 
