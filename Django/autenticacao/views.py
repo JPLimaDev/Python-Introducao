d1a41cfc827881bf06d98ac5d6682bbf00e6872a
Joaopaulo200317 <joaopaulodamotalima@gmail.com>
2026-01-29 16:23:42 -0300

    Enviando arquivo django das aulas

--- Django/autenticacao/views.py
+++ Django/autenticacao/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from .models import Pessoa
+
 def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro/index.html')
     
-    return render(request, 'cadastro/index.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        pessoa = Pessoa(
+            nome = nome, 
+            email = email,
+            senha = senha
+        )
+        pessoa.save()
+        return HttpResponse('Enviado os dados')
+        
+def listar(request):
+    pessoa = Pessoa.objects.filter(nome = 'Matheus').filter(email = 'joaopaulo@gmail.com')
+    pessoa.delete()
+    pessoas = Pessoa.objects.all()
+    return render(request, 'listar/listar.html', {'pessoas':pessoas})
 
-def valida_formulario(request):
-    nome = request.GET.get('nome')
-    email = request.GET.get('email')
+    
 
-    return HttpResponse(json.dumps({'nome': nome, 'email': email}))
+
 
 
